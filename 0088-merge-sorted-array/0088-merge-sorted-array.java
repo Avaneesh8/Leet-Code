1dffc4c01b3b4b7f105bd8ec2872577581eaf49d
AVANEESH MAHESHWARI <94730915+Avaneesh8@users.noreply.github.com>
2023-09-26 18:15:38 +0530

    Time: 2 ms (8.21%), Space: 41.4 MB (38.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums,int n){
-        int i=0;
+        /*int i=0;
         int a=0;
         int ar[]=new int[m+n];
         for(int x=0;x<(m+n);x++)
@@ -31,5 +31,38 @@ class Solution {
         {
             nums1[x]=ar[x];
         }
+    }*/
+    if(m<=0)
+    {
+                for(int x=0;x<n;x++)
+        {
+            nums1[x+m]=nums[x];
+        }
+    }
+    else if(n<=0)
+    {
+
+    }
+    else
+    {
+    for(int x=0;x<(m);x++)
+    {
+        if(nums1[x]<=nums[0])
+        {
+            nums1[x]=nums1[x];
+        }
+        else
+        {
+            int temp=nums[0];
+            nums[0]=nums1[x];
+            nums1[x]=temp;Arrays.sort(nums);
+            //System.out.println(nums[x]);
+        }
+    }
+    for(int x=0;x<n;x++)
+    {
+        nums1[x+m]=nums[x];
+    }
+    }
     }
 }
