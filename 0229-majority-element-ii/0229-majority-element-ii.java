f632cfc842b46d7e522495d28836b4754da4212c
AVANEESH MAHESHWARI <94730915+Avaneesh8@users.noreply.github.com>
2023-09-26 15:41:42 +0530

    Time: 12 ms (33.22%), Space: 46 MB (94.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -8,22 +8,19 @@ class Solution {
             
             if(hashtable.containsKey(nums[x]))
             {
-                int i=hashtable.get(nums[x])+1;
-                hashtable.put(nums[x],i);
+                hashtable.put(nums[x],hashtable.get(nums[x])+1);
             }
             else
             {
-                hashtable.putIfAbsent(nums[x],1);
+                hashtable.put(nums[x],1);
             }  
         }
-   for(Map.Entry<Integer, Integer> m : hashtable.entrySet()){    
-    System.out.println(m.getKey()+" "+m.getValue());    
-    if((m.getValue())>n/3)
-    {
-        listint.add(m.getKey());
-    }
-   } 
-
+        for(Map.Entry<Integer, Integer> m : hashtable.entrySet()){      
+            if((m.getValue())>n/3)
+            {
+                listint.add(m.getKey());
+            }
+        } 
         return listint;
     }
 }
